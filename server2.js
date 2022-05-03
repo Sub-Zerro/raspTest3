6f8dd1490e98c66a70ee5645bf2d6f6420c202a0
Roman <jimi4kuryki@gmail.com>
2022-05-03 16:08:46 +0500

    newNameOfPage

--- server2.js
+++ server2.js
@@ -403,7 +403,7 @@ const server = http.createServer((req, res) => {
             <meta charset="UTF-8">
                          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
                                      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                         <title>Document</title>
+                         <title>Schedule 17 of the Gymnasium</title>
                          <style>
 
                             body{
